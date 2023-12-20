2e8356ff0c3f33b73211330c67939a3546671cbb
Student10 <student10@Codes-MacBook-Air-85.local>
2023-12-20 11:48:50 -0500

    commit with errors

---
+++ src/test/java/com/abeeks/employeemgrserver/EmployeeMgrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeeks.employeemgrserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
